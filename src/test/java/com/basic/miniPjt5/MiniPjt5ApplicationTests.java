d9288df5952cf5f8b46b7c25e90af9774bda9e5a
CECHERI <24cecheri@gmail.com>
2025-05-26 16:43:57 +0900

    first

---
+++ src/test/java/com/basic/miniPjt5/MiniPjt5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.basic.miniPjt5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniPjt5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
