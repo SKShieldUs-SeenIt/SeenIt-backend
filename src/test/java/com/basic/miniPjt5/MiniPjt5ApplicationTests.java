b7b6de509156a2a9d613bc4cd840b1a610fc3736
UN15 <jaeun101501@gmail.com>
2025-05-26 16:53:12 +0900

    connect

--- src/test/java/com/basic/miniPjt5/MiniPjt5ApplicationTests.java
+++ src/test/java/com/basic/miniPjt5/MiniPjt5ApplicationTests.java
@@ -2,10 +2,15 @@ package com.basic.miniPjt5;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+<<<<<<< HEAD
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
 @ActiveProfiles("test")
+=======
+
+@SpringBootTest
+>>>>>>> 4de31b1 (connect)
 class MiniPjt5ApplicationTests {
 
 	@Test
