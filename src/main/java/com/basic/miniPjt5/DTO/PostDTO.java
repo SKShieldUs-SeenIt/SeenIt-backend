d27f1286650aadd9de4f4353ac7ab59b9f0d1982
UN15 <jaeun101501@gmail.com>
2025-06-04 14:40:46 +0900

    feat: swagger 추가 + fix: delete 시 userId 확인 후 제거로 수정

--- src/main/java/com/basic/miniPjt5/DTO/PostDTO.java
+++ src/main/java/com/basic/miniPjt5/DTO/PostDTO.java
@@ -3,6 +3,7 @@ package com.basic.miniPjt5.DTO;
 import com.basic.miniPjt5.entity.Post;
 import com.basic.miniPjt5.entity.User;
 import com.basic.miniPjt5.enums.ContentType;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
@@ -15,23 +16,29 @@ import org.springframework.web.multipart.MultipartFile;
 import java.time.LocalDateTime;
 
 public class PostDTO {
+    @Schema(description = "게시글 생성 DTO")
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
     public static class createRequest {
+        @Schema(description = "게시글 제목")
         @NotBlank(message = "제목은 필수 입력사항 입니다.")
         @Size(max = 100, message = "제목은 100자 이상 작성할 수 없습니다.")
         private String title;
 
+        @Schema(description = "게시글 내용")
         @NotBlank(message = "내용은 필수 입력사항 입니다.")
         @Size(max = 255, message = "내용은 255자 이상 작성할 수 없습니다.")
         private String body;
 
+        @Schema(description = "게시글 이미지")
         private MultipartFile image;
 
+        @Schema(description = "연결된 컨텐츠 종류", allowableValues = {"MOVIE", "DRAMA"})
         private ContentType contentType;
 
+        @Schema(description = "연결된 컨텐츠의 Id값")
         private Long contentId;
 
         public Post toEntity(String imageUrl, User user) {
@@ -46,37 +53,57 @@ public class PostDTO {
         }
     }
 
+    @Schema(description = "게시글 수정 DTO")
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
     public static class updateRequest {
+        @Schema(description = "게시글 제목")
         @NotBlank(message = "제목은 필수 입력사항 입니다.")
         @Size(max = 100, message = "제목은 100자 이상 작성할 수 없습니다.")
         private String title;
 
+        @Schema(description = "게시글 내용")
         @NotBlank(message = "내용은 필수 입력사항 입니다.")
         @Size(max = 255, message = "내용은 255자 이상 작성할 수 없습니다.")
         private String body;
 
+        @Schema(description = "게시글 이미지")
         private MultipartFile image;
 
         private boolean deleteExistingImage;
 
     }
 
+    @Schema(description = "게시글 목록 Response DTO")
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
     public static class ListResponse {
+        @Schema(description = "게시글 Id")
         private Long id;
+
+        @Schema(description = "게시글 코드값", example = "'P'+yymmdd+순서")
         private String code;
+
+        @Schema(description = "게시글 제목")
         private String title;
+
+        @Schema(description = "게시글 내용")
         private String body;
+
+        @Schema(description = "게시글 이미지 주소")
         private String imageUrl;
+
+        @Schema(description = "연결된 컨텐츠 종류", allowableValues = {"MOVIE", "DRAMA"})
         private ContentType contentType;
+
+        @Schema(description = "연결된 컨텐츠의 Id값")
         private Long contentId;
+
+        @Schema(description = "작성한 사용자")
         private UserDTO.SimpleResponse user;
 
         public static ListResponse fromEntity(Post post) {
@@ -92,20 +119,40 @@ public class PostDTO {
         }
     }
 
+    @Schema(description = "게시글 단건 Response DTO")
     @Data
     @NoArgsConstructor
     @AllArgsConstructor
     @Builder
     public static class Response {
+        @Schema(description = "게시글 Id")
         private Long id;
+
+        @Schema(description = "게시글 코드값", example = "'P'+yymmdd+순서")
         private String code;
+
+        @Schema(description = "게시글 제목")
         private String title;
+
+        @Schema(description = "게시글 내용")
         private String body;
+
+        @Schema(description = "게시글 이미지 주소")
         private String imageUrl;
+
+        @Schema(description = "연결된 컨텐츠 종류", allowableValues = {"MOVIE", "DRAMA"})
         private ContentType contentType;
+
+        @Schema(description = "연결된 컨텐츠의 Id값")
         private Long contentId;
+
+        @Schema(description = "생성 시간")
         private LocalDateTime createdAt;
+
+        @Schema(description = "수정 시간")
         private LocalDateTime updatedAt;
+
+        @Schema(description = "작성한 사용자")
         private UserDTO.SimpleResponse user;
 
         public static Response fromEntity(Post post) {
---
+++ src/main/java/com/basic/miniPjt5/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.basic.miniPjt5.config;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@OpenAPIDefinition(
+        info = @Info(title = "SeenIt 백엔드 API",
+                description = "SeenIt 백엔드 API 문서",
+                version = "1.0.0")
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        type = SecuritySchemeType.HTTP,
+        scheme = "bearer",
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER,
+        description = "JWT 인증 토큰을 입력하세요. 예: Bearer {token}"
+)
+public class SwaggerConfig {
+}
